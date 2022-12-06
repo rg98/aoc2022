a7e624679b75ae4bed00400e9e872ded208ff68a
Ralph Ganszky <ralph.ganszky@sap.com>
2022-12-06 17:15:20 +0100

    Beautify 4-1

--- 4-1.py
+++ 4-1.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+from collections import Counter
+
 count = 0
 
 with open('in.4', 'r') as fd:
@@ -7,16 +9,10 @@ with open('in.4', 'r') as fd:
         a, b = line[:-1].split(',')
         a_1, a_2 = a.split('-')
         b_1, b_2 = b.split('-')
-        a_1 = int(a_1)
-        a_2 = int(a_2)
-        b_1 = int(b_1)
-        b_2 = int(b_2)
-        print(a_1, a_2, b_1, b_2)
-        if a_1 <= b_1 and a_2 >= b_2:
-            print('b in a')
-            count += 1
-        elif a_1 >= b_1 and a_2 <= b_2:
-            print('a in b')
-            count += 1
+        c_a = Counter(range(int(a_1), int(a_2) + 1))
+        c_b = Counter(range(int(b_1), int(b_2) + 1))
+        if len(list(c_a & c_b)) == len(list(c_a)) or \
+           len(list(c_a & c_b)) == len(list(c_b)):
+           count += 1
 
 print(count)
