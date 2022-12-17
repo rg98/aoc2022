ba4fe0480fe85f2fc7c038411f9475da81567b0c
Ralph Ganszky <ralph@ganszky.de>
2022-12-17 13:08:08 +0100

    Added day ten

--- 10-1.py
+++ 10-1.py
@@ -12,24 +12,24 @@ result = 0
 with open('in.10', 'r') as fd:
     for instruction in fd:
         code = instruction.split()
-        print(code)
+        # print(code)
         if len(code) == 1:
             opcode = code[0]
             if ticks + 1 >= check:
                 result += x * check
                 check += check_interval
-                print(f'- ticks: {ticks+1}, x: {x}, check: {check}, result: {result}')
+                # print(f'- ticks: {ticks+1}, x: {x}, check: {check}, result: {result}')
             ticks += 1
-            print(f'ticks: {ticks}, x: {x}, check: {check}')
+            # print(f'ticks: {ticks}, x: {x}, check: {check}')
         else:
             opcode, argument = code
             if ticks + 2 >= check:
                 result += x * check
                 check += check_interval
-                print(f'- ticks: {ticks+2}, x: {x}, check: {check}, result: {result}')
+                # print(f'- ticks: {ticks+2}, x: {x}, check: {check}, result: {result}')
             ticks += 2
             x += int(argument)
-            print(f'ticks: {ticks}, x: {x}, check: {check}')
+            # print(f'ticks: {ticks}, x: {x}, check: {check}')
 
 print('ticks:', ticks)
 print('x:', x)
